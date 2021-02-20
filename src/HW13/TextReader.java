11b7199dae84b62fcb73dfec010dce76ea6c8d58
Sergey Hovhannisyan <sergohovhyan@gmail.com>
2021-02-20 21:52:49 +0400

    HW13
    writing sorted array to new file

--- src/HW13/TextReader.java
+++ src/HW13/TextReader.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class TextReader {
     public static void main(String[] args) {
         File text = new File("test.txt");
+        String[] split = new String[1];
         try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(text))) {
             bufferedOutputStream.write("200, 300, 100".getBytes());
         } catch (IOException e) {
@@ -15,7 +16,7 @@ public class TextReader {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(text));
             String line = bufferedReader.readLine();
             while(line != null) {
-                String[] split = line.split(", ");
+                split = line.split(", ");
                 System.out.println("unsorted");
                 System.out.println(Arrays.toString(split));
                 Arrays.sort(split);
@@ -23,6 +24,11 @@ public class TextReader {
                 System.out.println(Arrays.toString(split));
                 line = bufferedReader.readLine();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("blaSorted.txt"))) {
+            bufferedOutputStream.write(Arrays.toString(split).getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
